f69e7cba38d2eff64fa6333f545c805757a7db77
Yuriy Stul <ysden123@gmail.com>
2024-05-26 10:56:44 +0300

    1st version - empty dir list

---
+++ YSFileToolsCS/EmptyDirsControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace YSFileToolsCS
+{
+    /// <summary>
+    /// Interaction logic for EmptyDirsControl.xaml
+    /// </summary>
+    public partial class EmptyDirsControl : UserControl
+    {
+        public EmptyDirsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            dynamic dialog = new OpenFolderDialog();
+            dialog.Title = "Choose initial directory";
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                DirectoryText.Text = dialog.FolderName;
+            }
+        }
+
+        private void FindButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DirectoryText.Text.Length == 0)
+            {
+                EmptyListText.Text = "";
+                return;
+            }
+
+            EmptyListText.Text = "Waiting...";
+
+            var enumerationOptions = new EnumerationOptions();
+            enumerationOptions.RecurseSubdirectories = true;
+
+            var directories = Directory.EnumerateDirectories(DirectoryText.Text, "*", enumerationOptions);
+            EmptyListText.Text = "";
+
+            foreach (var directory in directories)
+            {
+                if (!Directory.EnumerateFileSystemEntries(directory).Any())
+                {
+                    EmptyListText.Text += directory;
+                    EmptyListText.Text += "\n";
+                }
+            }
+        }
+    }
+}
---
+++ YSFileToolsCS/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace YSFileToolsCS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+            if (fvi != null && fvi.FileVersion != null)
+            {
+                string version = fvi.FileVersion;
+                Title = $"{Title} {version}";
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItem_EmptyDirs_Click(object sender, RoutedEventArgs e)
+        {
+            CC.Content = new EmptyDirsControl();
+        }
+    }
+}
